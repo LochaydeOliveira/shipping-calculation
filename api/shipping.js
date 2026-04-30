5e99611073ce8285adaf8c3a19e9e0613f36ef44
Lochayde Oliveira <lochaydeguerreiro@hotmail.com>
2026-04-30 13:52:34 -0300

    feat: teste 2

--- api/shipping.js
+++ api/shipping.js
@@ -91,13 +91,13 @@ export default async function handler(req, res) {
     }
 
 
-
+    
     // =========================
     // RESPOSTA FINAL
     // =========================
 
     return res.status(200).json({
-    raw: data
+        raw: data
     });
 
   } catch (error) {
